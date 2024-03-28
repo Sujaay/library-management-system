eef89c2fdff9e16c737aff35246ac23262b628b1
Sujaay <sujay.kumar47@gmail.com>
2024-03-28 10:53:22 +0530

    login page to user_dashboard with backend

--- application/__init__.py
+++ application/__init__.py
@@ -3,13 +3,17 @@ from application.database import db
 from application.models import *
 
 app = Flask(__name__, template_folder="../templates", static_folder='../static')
-app.config['secert_key'] = 'aush33rf4r2gflashfaoi'
+app.config['SECRET_KEY'] = 'aush33rf4r2gflashfaoi'  # Fix typo: 'secert_key' should be 'SECRET_KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
 
 db.init_app(app)
 
+# No need to use 'with app.app_context()' for 'db.create_all()' here, as it's not necessary during app creation
+
+# Ensure you have imported all models so that db.create_all() can create tables for them
+
+# For example:
+from application.models import User  # Import your User model
+
 with app.app_context():
     db.create_all()
-
-
- 
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from application import app
 from application.models import User
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 from application.database import db
 
 @app.route('/')
@@ -32,11 +32,39 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(email=email, password=password).first()
         if user:
-            print("Login Sucessful and logged in user", user)
-            return redirect(url_for('index'))
+            session['user_id'] = user.id  # Store user ID in session upon successful login
+            print("Login Successful and logged in user", user)
+            return redirect(url_for('user_dashboard'))
         else:
             print("Wrong password or email")
             return render_template('login.html')
+        
+@app.route('/user_dashboard')
+def user_dashboard():
+    # Ensure user_id is in session before fetching user details
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get(user_id)
+        if user:
+            name = user.name
+            email = user.email
+            phone = user.phone
+            address = user.address
+            return render_template('user_dashboard.html', name=name, email=email, phone=phone, address=address)
+    # If user_id is not in session or user does not exist, redirect to login page
+    return redirect(url_for('login'))
 
-def register():
-    print("Registering user")
+@app.route('/user_profile', methods=['GET', 'POST'])
+def user_profile():
+    # Ensure user_id is in session before fetching user details
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get(user_id)
+        if user:
+            name = user.name
+            email = user.email
+            phone = user.phone
+            address = user.address
+            return render_template('user_profile.html', name=name, email=email, phone=phone, address=address)
+    # If user_id is not in session or user does not exist, redirect to login page
+    return redirect(url_for('login'))
